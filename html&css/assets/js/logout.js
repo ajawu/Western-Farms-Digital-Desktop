5d05dcc8994a601fb0cc4854b1b9e64420c0d33d
David Ajawu <ajawudavid@gmail.com>
2021-07-17 08:37:49 +0100

    fix: fixed flicker issue of admin elements on non admin user

--- html&css/assets/js/logout.js
+++ html&css/assets/js/logout.js
@@ -5,3 +5,10 @@ document.getElementById('window-logout-button').addEventListener('click', () => 
   remote.getCurrentWindow().loadFile('html&css/pages/auth/sign-in.html');
 });
 
+const isAdmin = JSON.parse(window.localStorage.getItem('auth')).admin;
+const adminElments = document.getElementsByClassName('admin-only-button');
+if (parseInt(isAdmin, 10) === 1) {
+  for (const adminElement of adminElments) {
+    adminElement.classList.remove('d-none');
+  }
+}
